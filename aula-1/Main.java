a750a394af016c91281fd7664ec3089d6da30775
nickolas <nickfcolovatti2020@gmail.com>
2026-04-25 11:29:48 -0300

    Add files via upload

---
+++ aula-1/Main.java
@@ -0,0 +1,20 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+
+ int idadePessoaA = 22;
+ int idadePessoaB = 25;
+ boolean mesmaIdade = idadePessoaB == idadePessoaA;
+ boolean maiorde18 = idadePessoaA > 18;
+ 
+ 
+ 
+boolean diferente = idadePessoaA != idadePessoaB;
+boolean menorDe12 = idadePessoaB < 12;
+boolean 
+
+ 
+	
+	}
+}
