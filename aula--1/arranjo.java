3bb47078232ed7ba941a21e9830ba11dd9551ccf
nickolas <nickfcolovatti2020@gmail.com>
2026-06-13 11:16:53 -0300

    Add files via upload

---
+++ aula--1/arranjo.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner (System.in);
+	    
+		int abeba [] ={2,5,7};
+		int clovis = abeba[0] + abeba[1] + abeba[2];
+		System.out.println(clovis);
+		
+		int j = sc.nextInt();
+		int jonas []= new int[j];
+		for(int i = 0;i < jonas.length;i++)
+		{
+		    jonas[i]= sc.nextInt();
+		    
+		};
+		
+		
+		
+		
+		
+	}
+}
