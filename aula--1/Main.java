2acaf7fa0e3d8030b5fd4d33db91f918d1601f65
nickolas <nickfcolovatti2020@gmail.com>
2026-06-27 11:26:07 -0300

    Add files via upload

--- aula--1/Main.java
+++ aula--1/Main.java
@@ -2,30 +2,24 @@ import java.util.*;
 public class Main
 {
 	public static void main(String[] args) {
-	    Scanner sc = new Scanner (System.in);
-	    int lin = 2;
-	    int col = 3;
-	int[][] tabela = new int[lin][col];
-	tabela[0][0]= 9;
-	tabela[0][1]= 7;
-	tabela[0][2]= 3;
-	tabela[1][0]= 7;
-	tabela[1][1]= 2;
-	tabela[1][2]= 8;
-	
-	int soma = (tabela[0][1] + tabela[0][0] + tabela[0][2])/3;
-	System.out.println("aluno 1: " + soma + " " + "aluno 2: " + (tabela[1][0] + tabela[1][1] + tabela[1][2])/3);
-	
-	//imprimindo notas
-	for( int i = 0; i<lin; i++)
-	{
-	    for(int j = 0; j<col; j++)
-	    {
-	      System.out.print(tabela[i][j]);  
-	        
-	    }
-	    System.out.print(" "); 
-	  }
-	}
-	
+    Scanner sc = new Scanner (System.in);
+    String as = sc.nextLine();
+    int numero = as.length();
+    System.out.println("olá " + as + ", seu nome tem " + numero + " caracteres");
+    System.out.println("o");
+    System.out.println(as.charAt(1));
+    if (as.equals("nickolas"))
+    {
+        System.out.println("parabén, você acertou!");
+    }
+    else
+    {
+        System.out.println("Que pena, você errou!");
+    }
+   String[] sobrenome = as.split(" ");
+   System.out.println(sobrenome[0]);
+   
+    
+    
+  }
 }
